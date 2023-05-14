b50b92a512e7d9ee45f3c11785a9b5618a0ff523
Dmitry O. Markov <markov.dm@inbox.ru>
2023-05-14 13:37:50 +0300

    add overlap validation and add I18n to validations

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,7 @@
 class Play < ApplicationRecord
   validates :title, presence: true
   validate :valid_date_range
+  validate :validate_other_plays_overlap
 
   def start_date
     date_range.first
@@ -23,7 +24,13 @@ class Play < ApplicationRecord
   def valid_date_range
     unless date_range.is_a?(Range) && date_range.begin.is_a?(Date) && date_range.end.is_a?(Date) &&
       date_range.begin < date_range.end
-      errors.add(:date_range, "must be a valid date range with a beginning date smaller than its end date")
+      errors.add(:date_range, :invalid_date_range)
     end
+  end
+
+  def validate_other_plays_overlap
+    sql = "date_range && daterange(:start_date, :end_date)"
+    is_overlapping = Play.where(sql, start_date: date_range.begin, end_date: date_range.end).exists?
+    errors.add(:date_range, :overlaps_with_other) if is_overlapping
   end
 end
